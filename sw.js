53472321c9905f459f4f01f85685956da371c400
webstudiohub <pessoalhdn123@gmail.com>
2026-07-19 02:43:34 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,19 @@
-const CACHE_NAME = 'gerna-transportes-v2';
+const CACHE_NAME = 'gerna-transportes-v3';
 const ASSETS = [
   './index.html',
   './manifest.json',
+  './gerna-logo.svg',
+  './favicon.ico',
+  './apple-touch-icon.png',
+  './icon-36.png',
+  './icon-48.png',
+  './icon-72.png',
+  './icon-96.png',
+  './icon-128.png',
+  './icon-144.png',
+  './icon-152.png',
   './icon-192.png',
+  './icon-384.png',
   './icon-512.png'
 ];
 
